34890aaeb1a2b96b6a26bb0e940675c69e5f9bda
VislavY <vladislav.yants@gmail.com>
2021-08-11 16:01:03 +0300

    Implement Retrofit2

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,4 +1,4 @@
-object Version {
+object Versions {
 
     const val ANDROIDX_CORE = "1.6.0"
     const val ANDROIDX_APPCOMPAT = "1.3.1"
@@ -8,6 +8,8 @@ object Version {
     const val GOOGLE_MATERIAL = "1.4.0"
     const val GOOGLE_DAGGER = "2.38.1"
 
+    const val SQUAREUP_RETROFIT = "2.9.0"
+
     const val VK_SDK = "2.2.0"
 
     const val JUNIT = "4.13.2"
@@ -16,21 +18,23 @@ object Version {
 }
 
 @Suppress("unused")
-object Depend {
+object Depends {
 
-    const val ANDROIDX_CORE = "androidx.core:core-ktx:${Version.ANDROIDX_CORE}"
-    const val ANDROIDX_APPCOMPAT = "androidx.appcompat:appcompat:${Version.ANDROIDX_APPCOMPAT}"
-    const val ANDROIDX_CONSTRAINTLAYOUT = "androidx.constraintlayout:constraintlayout:${Version.ANDROIDX_CONSTRAINTLAYOUT}"
-    const val ANDROIDX_NAVIGATION_FRAGMENT = "androidx.navigation:navigation-fragment-ktx:${Version.ANDROIDX_NAVIGATION}"
-    const val ANDROIDX_NAVIGATION_UI = "androidx.navigation:navigation-ui-ktx:${Version.ANDROIDX_NAVIGATION}"
+    const val ANDROIDX_CORE = "androidx.core:core-ktx:${Versions.ANDROIDX_CORE}"
+    const val ANDROIDX_APPCOMPAT = "androidx.appcompat:appcompat:${Versions.ANDROIDX_APPCOMPAT}"
+    const val ANDROIDX_CONSTRAINTLAYOUT = "androidx.constraintlayout:constraintlayout:${Versions.ANDROIDX_CONSTRAINTLAYOUT}"
+    const val ANDROIDX_NAVIGATION_FRAGMENT = "androidx.navigation:navigation-fragment-ktx:${Versions.ANDROIDX_NAVIGATION}"
+    const val ANDROIDX_NAVIGATION_UI = "androidx.navigation:navigation-ui-ktx:${Versions.ANDROIDX_NAVIGATION}"
 
-    const val GOOGLE_MATERIAL = "com.google.android.material:material:${Version.GOOGLE_MATERIAL}"
-    const val GOOGLE_DAGGER = "com.google.dagger:dagger:${Version.GOOGLE_DAGGER}"
-    const val GOOGLE_DAGGER_COMPILER = "com.google.dagger:dagger-compiler:${Version.GOOGLE_DAGGER}"
+    const val GOOGLE_MATERIAL = "com.google.android.material:material:${Versions.GOOGLE_MATERIAL}"
+    const val GOOGLE_DAGGER = "com.google.dagger:dagger:${Versions.GOOGLE_DAGGER}"
+    const val GOOGLE_DAGGER_COMPILER = "com.google.dagger:dagger-compiler:${Versions.GOOGLE_DAGGER}"
 
-    const val VK_SDK = "com.vk:androidsdk:${Version.VK_SDK}"
+    const val SQUAREUP_RETROFIT = "com.squareup.retrofit2:retrofit:${Versions.SQUAREUP_RETROFIT}"
 
-    const val JUNIT = "junit:junit:${Version.JUNIT}"
-    const val ANDROIDX_TEST_JUNIT = "androidx.test.ext:junit:${Version.ANDROIDX_TEST_JUNIT}"
-    const val ANDROIDX_TEST_ESPRESSO = "androidx.test.espresso:espresso-core:${Version.ANDROIDX_TEST_ESPRESSO}"
+    const val VK_SDK = "com.vk:androidsdk:${Versions.VK_SDK}"
+
+    const val JUNIT = "junit:junit:${Versions.JUNIT}"
+    const val ANDROIDX_TEST_JUNIT = "androidx.test.ext:junit:${Versions.ANDROIDX_TEST_JUNIT}"
+    const val ANDROIDX_TEST_ESPRESSO = "androidx.test.espresso:espresso-core:${Versions.ANDROIDX_TEST_ESPRESSO}"
 }
