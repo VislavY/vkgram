ce32fea80c85f31a423d1e5a0d99be031588b571
VislavY <vladislav.yants@gmail.com>
2021-08-22 12:58:00 +0300

    Fix: usernames were only displaying in English

--- app/src/main/java/ru/vyapps/vkgram/data/remote/requests/GetConversationsRequest.kt
+++ app/src/main/java/ru/vyapps/vkgram/data/remote/requests/GetConversationsRequest.kt
@@ -1,6 +1,9 @@
 package ru.vyapps.vkgram.data.remote.requests
 
-import com.vk.api.sdk.requests.VKRequest
+import com.vk.api.sdk.VKApiManager
+import com.vk.api.sdk.VKApiResponseParser
+import com.vk.api.sdk.VKMethodCall
+import com.vk.api.sdk.internal.ApiCommand
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import org.json.JSONObject
@@ -10,109 +13,126 @@ import ru.vyapps.vkgram.data.User
 import java.lang.Exception
 import kotlin.collections.ArrayList
 
-class GetConversationsRequest(count: Int) : VKRequest<List<Conversation>>("messages.getConversations") {
+class GetConversationsRequest(
+    private val count: Int
+): ApiCommand<List<Conversation>>() {
 
-    init {
-        addParam("count", count)
-        addParam("extended", 1)
-        addParam("fields", "photo_200")
+    override fun onExecute(manager: VKApiManager): List<Conversation> {
+
+        val methodCall = VKMethodCall.Builder()
+            .method("messages.getConversations")
+            .args("lang", "ru")
+            .args("extended", 1)
+            .args("count", count)
+            .args("fields", "photo_200")
+            .version(manager.config.version)
+            .build()
+        return manager.execute(methodCall, Parser())
     }
 
-    override fun parse(r: JSONObject): List<Conversation> {
-        val responseJObj = r.getJSONObject("response")
-        val json = Json { ignoreUnknownKeys = true }
+    private class Parser: VKApiResponseParser<List<Conversation>> {
 
-        val itemsJArray = responseJObj.getJSONArray("items")
-        val conversations = ArrayList<Conversation>()
-        for (i in 0 until itemsJArray.length()) {
-            val itemJObj = itemsJArray.getJSONObject(i)
-            val conversationJObj = JSONObject()
-            conversationJObj.put(
-                "id",
-                itemJObj
-                    .getJSONObject("conversation")
-                    .getJSONObject("peer")
-                    .getLong("id")
-            )
-            val type = itemJObj
-                .getJSONObject("conversation")
-                .getJSONObject("peer")
-                .getString("type")
-            conversationJObj.put("type", type)
+        override fun parse(response: String?): List<Conversation> {
+            val conversations = ArrayList<Conversation>()
 
-            if (type == "chat") {
-                try {
+            response?.let {
+                val responseJObj = JSONObject(it).getJSONObject("response")
+                val json = Json { ignoreUnknownKeys = true }
+
+                val itemsJArray = responseJObj.getJSONArray("items")
+                for (i in 0 until itemsJArray.length()) {
+                    val itemJObj = itemsJArray.getJSONObject(i)
+                    val conversationJObj = JSONObject()
                     conversationJObj.put(
-                        "avatar",
+                        "id",
                         itemJObj
                             .getJSONObject("conversation")
-                            .getJSONObject("chat_settings")
-                            .getJSONObject("photo")
-                            .getString("photo_200")
+                            .getJSONObject("peer")
+                            .getLong("id")
                     )
-                } catch (e: Exception) {}
-
-                conversationJObj.put(
-                    "title",
-                    itemJObj
+                    val type = itemJObj
                         .getJSONObject("conversation")
-                        .getJSONObject("chat_settings")
-                        .getString("title")
-                )
-            }
+                        .getJSONObject("peer")
+                        .getString("type")
+                    conversationJObj.put("type", type)
 
-            conversationJObj.put(
-                "last_message",
-                itemJObj
-                    .getJSONObject("last_message")
-                    .getString("text")
-            )
-            conversationJObj.put(
-                "last_message_date",
-                itemJObj
-                    .getJSONObject("last_message")
-                    .getLong("date")
-            )
+                    if (type == "chat") {
+                        try {
+                            conversationJObj.put(
+                                "avatar",
+                                itemJObj
+                                    .getJSONObject("conversation")
+                                    .getJSONObject("chat_settings")
+                                    .getJSONObject("photo")
+                                    .getString("photo_200")
+                            )
+                        } catch (e: Exception) {
+                        }
 
-            val conversation = json.decodeFromString<Conversation>(conversationJObj.toString())
-            conversations.add(conversation)
-        }
+                        conversationJObj.put(
+                            "title",
+                            itemJObj
+                                .getJSONObject("conversation")
+                                .getJSONObject("chat_settings")
+                                .getString("title")
+                        )
+                    }
 
-        val profilesJArray = responseJObj.getJSONArray("profiles")
-        val users = ArrayList<User>()
-        for (i in 0 until profilesJArray.length()) {
-            val profileJObj = profilesJArray.getJSONObject(i)
-            val user = json.decodeFromString<User>(profileJObj.toString())
-            users.add(user)
-        }
+                    conversationJObj.put(
+                        "last_message",
+                        itemJObj
+                            .getJSONObject("last_message")
+                            .getString("text")
+                    )
+                    conversationJObj.put(
+                        "last_message_date",
+                        itemJObj
+                            .getJSONObject("last_message")
+                            .getLong("date")
+                    )
 
-        val groupsJArray = responseJObj.getJSONArray("groups")
-        val groups = ArrayList<Group>()
-        for (i in 0 until groupsJArray.length()) {
-            val groupJObj = groupsJArray.getJSONObject(i)
-            val group = json.decodeFromString<Group>(groupJObj.toString())
-            groups.add(group)
-        }
+                    val conversation = json.decodeFromString<Conversation>(conversationJObj.toString())
+                    conversations.add(conversation)
+                }
 
-        // TODO: Needs optimization
-        for (conversation in conversations) {
-            if (conversation.type == "user") {
-                for (user in users) {
-                    if (conversation.id == user.id) {
-                        conversation.title = "${user.first_name} ${user.last_name}"
-                        conversation.avatar = user.photo_200
+                val profilesJArray = responseJObj.getJSONArray("profiles")
+                val users = ArrayList<User>()
+                for (i in 0 until profilesJArray.length()) {
+                    val profileJObj = profilesJArray.getJSONObject(i)
+                    val user = json.decodeFromString<User>(profileJObj.toString())
+                    users.add(user)
+                }
+
+                val groupsJArray = responseJObj.getJSONArray("groups")
+                val groups = ArrayList<Group>()
+                for (i in 0 until groupsJArray.length()) {
+                    val groupJObj = groupsJArray.getJSONObject(i)
+                    val group = json.decodeFromString<Group>(groupJObj.toString())
+                    groups.add(group)
+                }
+
+                // TODO: Needs optimization
+                for (conversation in conversations) {
+                    if (conversation.type == "user") {
+                        for (user in users) {
+                            if (conversation.id == user.id) {
+                                conversation.title = "${user.first_name} ${user.last_name}"
+                                conversation.avatar = user.photo_200
+                            }
+                        }
+                    } else if (conversation.type == "group") {
+                        for (group in groups) {
+                            if (conversation.id == -group.id) {
+                                conversation.title = group.name
+                                conversation.avatar = group.photo_200
+                            }
+                        }
                     }
                 }
-            } else if (conversation.type == "group") {
-                for (group in groups) {
-                    if (conversation.id == -group.id) {
-                        conversation.title = group.name
-                        conversation.avatar = group.photo_200
-                    }
-                }
             }
-        }
 
-        return conversations
+
+            return conversations
+        }
     }
 }
