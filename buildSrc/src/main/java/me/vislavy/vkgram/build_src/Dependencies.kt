6455746cc9fd384c5a4d0467c248a68ddc32bf36
vislavy <vladislav.yants@gmail.com>
2022-01-08 00:52:26 +0300

    Update kotlin, compose and other dependencies

--- buildSrc/src/main/java/me/vislavy/vkgram/build_src/Dependencies.kt
+++ buildSrc/src/main/java/me/vislavy/vkgram/build_src/Dependencies.kt
@@ -3,11 +3,11 @@ package me.vislavy.vkgram.build_src
 @Suppress("UNUSED")
 object Libs {
     object AndroidX {
-        const val Appcompat = "androidx.appcompat:appcompat:1.3.0"
-        const val CoreKtx = "androidx.core:core-ktx:1.6.0"
+        const val Appcompat = "androidx.appcompat:appcompat:1.4.0"
+        const val CoreKtx = "androidx.core:core-ktx:1.7.0"
 
         object Compose {
-            const val Version = "1.1.0-beta03"
+            const val Version = "1.1.0-rc01"
             const val Material = "androidx.compose.material:material:$Version"
             const val MaterialIconsExtended = "androidx.compose.material:material-icons-extended:$Version"
             const val Animation = "androidx.compose.animation:animation:$Version"
@@ -15,7 +15,7 @@ object Libs {
         }
 
         object Hilt {
-            private const val Version = "2.38.1"
+            private const val Version = "2.40.5"
             const val HiltAndroid = "com.google.dagger:hilt-android:$Version"
             const val HiltNavigationCompose = "androidx.hilt:hilt-navigation-compose:1.0.0-alpha03"
             const val HiltCompiler = "com.google.dagger:hilt-compiler:$Version"
@@ -27,7 +27,7 @@ object Libs {
         }
 
         object Navigation {
-            const val NavigationCompose = "androidx.navigation:navigation-compose:2.4.0-beta02"
+            const val NavigationCompose = "androidx.navigation:navigation-compose:2.4.0-rc01"
         }
 
         object Room {
@@ -43,7 +43,7 @@ object Libs {
     }
 
     object Accompanist {
-        private const val Version = "0.21.3-beta"
+        private const val Version = "0.22.0-rc"
         const val AccompanistNavigationAnimation = "com.google.accompanist:accompanist-navigation-animation:$Version"
         const val AccompanistSystemUiController = "com.google.accompanist:accompanist-systemuicontroller:$Version"
         const val AccompanistPlaceholder = "com.google.accompanist:accompanist-placeholder-material:$Version"
@@ -56,8 +56,8 @@ object Libs {
     }
 
     object KotlinX {
-        const val KotlinXCoroutinesCore = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1"
-        const val KotlinXSerializationJson = "org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.2"
+        const val KotlinXCoroutinesCore = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0"
+        const val KotlinXSerializationJson = "org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2"
     }
 
     object Retrofit {
